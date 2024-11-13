3b9a6c44ddf5c076604e7de85f628deabba553da
nairobi2002 <nairobi.bodden@vfco.hn>
2024-11-13 14:20:35 -0600

    Initial commit

---
+++ handson08.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int8 = 127
+	var b uint8 = 255
+
+	fmt.Printf("a: %v is of type %T\n", a, a)
+	fmt.Printf("b: %v is of type %T\n", b, b)
+
+}
